4a65924e627e5ec8236a86ed0d05fd6a17c2b734
hariprasad09 <92730117+hariprasad09@users.noreply.github.com>
2023-01-02 22:23:30 +0530

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from 'src/app/home/welcome.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PlayerListComponent } from './player/player-list.component';
+import { RouterModule } from '@angular/router';
+import { PlayerDetailComponent } from './player/player-detail/player-detail.component';
+import { PlayerDetailGuard } from './player/player-detail/player-detail.guard';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PlayerRatingComponent } from './player-rating/player-rating.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PlayerListComponent,
+    PlayerDetailComponent,
+    PlayerRatingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path : 'welcome', component : WelcomeComponent},
+      {path : 'player', component : PlayerListComponent},
+      {
+        path : 'player/player-detail/:id',
+        canActivate : [PlayerDetailGuard],
+        component : PlayerDetailComponent
+      },
+      {path : '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path : '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ]),
+    NgbModule
   ],
   providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
